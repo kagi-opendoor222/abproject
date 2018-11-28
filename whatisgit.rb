b76b5d9b5955d9119ee778463aedb7625dfef07d
kagi-opendoor222 <opendoor222@gmail.com>
2018-11-28 20:52:36 +0900

    make show-method brief

--- whatisgit.rb
+++ whatisgit.rb
@@ -3,13 +3,16 @@ class WhatIsGit
     puts "https://#{lang}.wikipedia"
   end
   def show
-   puts "Do you understand the basis?[yes/no]"
-   input = gets.chomp.downcase
-   case input
+   case input("Do you understand the basis?[yes/no]")
    when "yes"
      puts "git is easy"
    else
      puts "git is difficult..."
    end
+  end
+  private
+  def input(message)
+    puts message
+    gets.chomp.downcase
   end
 end
