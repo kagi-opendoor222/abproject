073084bed0cc0c4a3e5e3a89bc9d3a336efe3c81
kagi-opendoor222 <opendoor222@gmail.com>
2018-12-10 11:37:10 +0900

    言語自動取得の準備

--- whatisgit.rb
+++ whatisgit.rb
@@ -1,5 +1,8 @@
 class WhatIsGit
-  def about(lang = "en")
+  def about(lang = nil)
+    if lang==nil then
+      lang = language
+    end
     puts "https://#{lang}.wikipedia2"
   end
   def show
@@ -10,7 +13,16 @@ class WhatIsGit
      puts "git is difficult..."
    end
   end
+  def aaa
+
+  end
+  def bbb
+
+  end
   private
+  def language
+
+  end 
   def input(message)
     puts message
     gets.chomp.downcase
